aca00e5d509e047ed2c1e2b21c5ca32ded203b86
Ali Solanki <alisolanki123@gmail.com>
2024-03-23 19:26:36 +0530

    latest

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,23 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 void main() {
   runApp(
     MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        useMaterial3: true,
-        colorScheme: kDarkColorScheme,
-        cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer,
-          ),
-        ),
-      ),
+      // darkTheme: ThemeData.dark().copyWith(
+      //   useMaterial3: true,
+      //   colorScheme: kDarkColorScheme,
+      //   cardTheme: const CardTheme().copyWith(
+      //     color: kDarkColorScheme.secondaryContainer,
+      //     margin: const EdgeInsets.symmetric(
+      //       horizontal: 16,
+      //       vertical: 8,
+      //     ),
+      //   ),
+      //   elevatedButtonTheme: ElevatedButtonThemeData(
+      //     style: ElevatedButton.styleFrom(
+      //       backgroundColor: kDarkColorScheme.primaryContainer,
+      //       foregroundColor: kDarkColorScheme.onPrimaryContainer,
+      //     ),
+      //   ),
+      // ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
